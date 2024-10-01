192b67e7b399013a2b985359f4eb9032599f803d
getReema <reeroo02@gmail.com>
2024-10-01 13:47:10 +0300

    Project init

---
+++ src/main/java/reema/com/AccountStatmentApplication.java
@@ -0,0 +1,13 @@
+package reema.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountStatmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountStatmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reema/com/AccountStatmentApplicationTests.java
@@ -0,0 +1,13 @@
+package reema.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountStatmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
